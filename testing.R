9dd5e091ed7c494456fefb755db2d6ca47a30c60
drtjc <curnock@gmail.com>
2018-06-13 22:44:56 -0500

    add unbinding and the garbage collector

--- testing.R
+++ testing.R
@@ -1,18 +1,1 @@
-rm(d)
-d <- data.frame(1)
-tracemem(t)
-t[1] <- 1
-untracemem(t)
 
-
-rm(l)
-l <- list(1)
-tracemem(l)
-l[1] <- 1
-untracemem(l)
-
-rm(v)
-v <- 1:3
-tracemem(v)
-v[3] <- 4L
-untracemem(v)
