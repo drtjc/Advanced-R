2f2ac5156109575bbdaeb314206a7e81550373a1
drtjc <curnock@gmail.com>
2018-06-20 22:32:36 -0500

    answer exercises

--- testing.R
+++ testing.R
@@ -10,36 +10,21 @@ combine(f1, f2)
 combine(list(f1, f2))
 
 
-a <- matrix(1:3, ncol = 3)
-a
-rownames(a) <- c("A")
-colnames(a) <- c("a", "b", "c")
-a
-dimnames(a)
-
-
 
 a <- matrix(1:6, nrow = 2, ncol = 3)
-rownames(a) <- c("A", "B")
-colnames(a) <- c("a", "b", "c")
-a
+class(a)
+is.matrix(a) # checks if has dim attribute of length 2
+str(a)
 
-f <- a[1, , drop = FALSE]
-f
-dim(f)
-names(f)
-rownames(f)
-colnames(f)
-dimnames(f)
-
-
-# One vector argument to describe all dimensions
 b <- array(1:12, c(2, 3, 2))
-dimnames(b) <- list(c("one", "two"), c("a", "b", "c"), c("A", "B"))
-b
+class(b)
+is.array(b)
+str(b)
 
-e <- b[1, 1, , drop = FALSE] # don't drop dimensions
-e # now an array
-dim(e)
-dimnames(e) # but still returns NULL
-names(e) # this works but is the vector option!
+df <- data.frame(1:2, 3:4)
+df
+dim(df)
+attributes(df)
+is.matrix(df)
+is.data.frame(df)
+
