5eb0b98aea38c54f6b4910c74fc3c2a91ff93498
Tom Curnock <tom@greenlightre.ky>
2018-06-23 13:21:10 -0500

    add data frames

--- testing.R
+++ testing.R
@@ -1,68 +1,36 @@
 library(lobstr)
 library(magrittr)
 library(tidyverse)
+library(purrr)
 
 df <- data.frame(x = 1:3, y = 3:1, z = letters[1:3], stringsAsFactors = FALSE)
-df
 str(df)
 
-# subsetting like a list
-(df[1]) # subsetting a list with [ always return a list
-is.data.frame(df[1]) # of 1 column - 1:3
-ncol(df[1])
-is.list(df[1]) # of one vector - 1:3
-length(df[1]) # a list of one item - 1:3
-df[[1]] # use [[ or $ to access a single item
-df[1][1]
-identical(df[1], df[1][1]) # TRUE since df[1] is a list of only one item
-df[c("x", "y")] # select multiple columns
-
-
-# subsetting like a matrix
-df[1, ]
-str(df[1, ]) # still a data frame
-df[, 1]
-str(df[, 1]) # now an int vector
-df[, 1, drop = FALSE]
-str(df[, 1, drop = FALSE]) # still a data frame
-df[1:2, 2:3]
-
-df[df[1] == 2, ] # still a data frame
-
-df[df[1] == 2]
-str(df[df[1] == 2]) # vector
-
-df[T]
-str(df[T])
-
+str(df[matrix(T)])
 
 df[matrix(T)]
-unlist(df)
 
-tt <- matrix(T)
-tt
-df[tt]
-str(df[tt])
+unlist(df, use.names = FALSE)
 
-df[1]
-class(df[1])
-df[1] == 2
-class(df[1] == 2)
-str(df[1] == 2)
-df[c(F, T, F)]
+df <- data.frame(x = 1:3, y = 3:1)
+df$z <- list(4:5, 4:6, 4:7)
+df
+str(df[matrix(T)])
+str(unlist(df, use.names = FALSE, recursive = FALSE))
+str(unlist(df, use.names = FALSE, recursive = TRUE))
+
+str(df[3])
+str(df[[3]])
+purrr::vec_depth(df[3])
+purrr::vec_depth(df[[3]])
 
 
-idx <- matrix(c(T, F, T, F, F, T, T, T, F), ncol = 3)
-idx
-df[idx]
 
-idx2 <- matrix(c(F, T, F), ncol = 1)
-idx2
-df[idx2]
-
-idx3 <- matrix(c(F, T, F), nrow = 1)
-idx3
-df[idx3]
+df <- data.frame(x = 1:3, y = 3:1, z = letters[1:3], stringsAsFactors = FALSE)
+df <- map(df, as.character)
+str(df)
+df[] <- map(df, as.character)
+str(df)
 
 
 
