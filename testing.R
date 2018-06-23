f44af208c76ab78d82ebf1a61e55a4f9eeaa81de
drtjc <curnock@gmail.com>
2018-06-22 20:01:50 -0500

    answer exercises

--- testing.R
+++ testing.R
@@ -2,28 +2,6 @@ library(lobstr)
 library(magrittr)
 
 
-a <- matrix(1:6, nrow = 2, ncol = 3)
-class(a)
-is.matrix(a) # checks if has dim attribute of length 2
-str(a)
-
-b <- array(1:12, c(2, 3, 2))
-class(b)
-is.array(b)
-str(b)
-
-df <- data.frame(1:2, 3:4)
-df
-dim(df)
-attributes(df)
-is.matrix(df)
-is.data.frame(df)
-
-
-
-
-
-
 
 
 
