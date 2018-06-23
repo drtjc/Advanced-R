e3a4c1303eccfd43909ce7128d17d53c10f6d376
drtjc <curnock@gmail.com>
2018-06-22 23:40:58 -0500

    test subsetting

--- testing.R
+++ testing.R
@@ -1,7 +1,68 @@
 library(lobstr)
 library(magrittr)
+library(tidyverse)
+
+df <- data.frame(x = 1:3, y = 3:1, z = letters[1:3], stringsAsFactors = FALSE)
+df
+str(df)
+
+# subsetting like a list
+(df[1]) # subsetting a list with [ always return a list
+is.data.frame(df[1]) # of 1 column - 1:3
+ncol(df[1])
+is.list(df[1]) # of one vector - 1:3
+length(df[1]) # a list of one item - 1:3
+df[[1]] # use [[ or $ to access a single item
+df[1][1]
+identical(df[1], df[1][1]) # TRUE since df[1] is a list of only one item
+df[c("x", "y")] # select multiple columns
 
 
+# subsetting like a matrix
+df[1, ]
+str(df[1, ]) # still a data frame
+df[, 1]
+str(df[, 1]) # now an int vector
+df[, 1, drop = FALSE]
+str(df[, 1, drop = FALSE]) # still a data frame
+df[1:2, 2:3]
+
+df[df[1] == 2, ] # still a data frame
+
+df[df[1] == 2]
+str(df[df[1] == 2]) # vector
+
+df[T]
+str(df[T])
+
+
+df[matrix(T)]
+unlist(df)
+
+tt <- matrix(T)
+tt
+df[tt]
+str(df[tt])
+
+df[1]
+class(df[1])
+df[1] == 2
+class(df[1] == 2)
+str(df[1] == 2)
+df[c(F, T, F)]
+
+
+idx <- matrix(c(T, F, T, F, F, T, T, T, F), ncol = 3)
+idx
+df[idx]
+
+idx2 <- matrix(c(F, T, F), ncol = 1)
+idx2
+df[idx2]
+
+idx3 <- matrix(c(F, T, F), nrow = 1)
+idx3
+df[idx3]
 
 
 
