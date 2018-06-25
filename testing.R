9b174c0c968a542a6437f0d29a0f9fe7edeaac37
drtjc <curnock@gmail.com>
2018-06-25 00:09:45 -0500

    testing

--- testing.R
+++ testing.R
@@ -2,8 +2,119 @@ library(lobstr)
 library(magrittr)
 library(tidyverse)
 library(purrr)
+library(pryr)
+
+c <- 10
+c(c = c)
 
 
+a <- NULL
+f <- function(a) 3 
+f(a <- 1) # not guaranteed to evaluate because of lazy evaluation
+a # a is still NULL
+f(a = 1) # this is a inside the function
+a # a is still NULL
+
+
+a <- NULL
+f <- function(a) a + 1 
+f(a <- 1) # evaluates because a argument of function is evaluated in the function
+a # a is 1
+f(a = 2) # returns 3
+a # a is still 1
+
+
+
+a <- NULL
+f <- function(a) a
+f(a <- 1) # does not display return value
+a # 1
+
+f(a = a <- 1) # does not display return value
+f(a = a <- 1 + 3) # does not display return value
+f(a = a <- (1)) # does not display return value
+
+
+
+
+f((a = a <- 1)) # displays
+f(a = (a <- 1)) # displays
+
+(f(a <- 1))
+
+
+## withVisible	do_withVisible	C:\R-3.1.2\src\main\eval.c
+
+
+withVisible(f(a <- 1))$visible
+withVisible(f(a = a <- 1))$visible
+withVisible(f(1))$visible
+withVisible(f(a = 1))$visible
+
+
+rinternals <- file.path(R.home("include"), "Rinternals.h")
+file.show(rinternals)
+
+
+withVisible(x <- 1)
+withVisible((x <- 1))
+
+
+
+
+a <- NULL
+f <- function(a) a 
+(f(a <- 1)) # forces display of return value
+a # 1
+
+a <- NULL
+f <- function(a) (a) 
+f(a <- 1) # return value is displayed
+a # 1
+
+a <- NULL
+f <- function(a) a 
+f((a <- 1)) # return value is displayed
+a # 1
+
+a <- NULL
+f <- function(a) a 
+f((a = 1)) # same as f(a <- 1) but with return value displayed
+a # 1
+
+# normal usage
+a <- NULL
+f <- function(a) a 
+f(a = 1) 
+a #NULL
+
+
+
+
+a <- NULL
+f <- function(a) a 
+f(a = force(a <- 1)) 
+a #1
+
+a <- NULL
+f <- function(a) a 
+f(a = a <- 1) 
+a #1
+
+
+
+
+
+
+foo = function(a) 0
+x = 1
+foo(a=force(x=2))
+x
+
+foo = function(a) deparse(substitute(a))
+x = 1
+foo(a=force(x=2))
+x
 
 
 
