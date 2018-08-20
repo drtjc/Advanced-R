36848b5cdf5e337617a77a360cc84816b40abe91
Tom Curnock <tom@greenlightre.ky>
2018-08-20 16:10:17 -0500

    stuff

--- testing.R
+++ testing.R
@@ -8,6 +8,37 @@ library(rlist)
 library(pryr)
 
 
+f4 <- function(x) substitute(x * 2)
+f4(a + b + c)
+f4(a)
+
+f5 <- function(x) enexpr(x * 2)
+f5(a + b + c)
+f5(a)
+
+
+
+name <- "tom"
+time <- "morning"
+
+dots <- exprs(Good, morning, tom)
+typeof(dots[[2]])
+paste(purrr::map(dots, expr_name), collapse = " ")
+
+dots <- exprs(Good, time, name)
+typeof(dots[[2]])
+paste(purrr::map(dots, expr_name), collapse = " ")
+
+dots <- exprs(Good, !!time, !!name)
+typeof(dots[[2]])
+paste(purrr::map(dots, expr_name), collapse = " ")
+
+time <- 33
+dots <- exprs(Good, !!time, !!name)
+typeof(dots[[2]])
+paste(purrr::map(dots, expr_name), collapse = " ")
+
+
 
 
 x <- quote(a + b)
@@ -247,53 +278,6 @@ f()
 # 
 # 
 # 
-# 
-# 
-# 
-# f <- function() {
-#   print("hi")
-#   recover()
-# }
-# 
-# 
-# 
-# #undebug(f)
-# #debug(f, text="here")
-# 
-# f()
-# class(sum)
-# class(trace(sum))
-# hist(rnorm(100)) # shows about 3-4 calls to sum()
-# untrace(sum)
-# 
-# 
-# # how to use debug in a nested function
-# 
-# isdebugged(f)
-# debuggingState(TRUE)
-# 
-# ?traceback()
-# 
-
-
-# f1 <- factor("a")
-# f2 <- factor("b")
-# c(f1, f2)
-# unlist(list(f1, f2))
-# 
-# combine(f1, f2)
-# combine(list(f1, f2))
-
-
-
-# ```{r, echo=FALSE}
-# d <- c("1d", "2d", "nd")
-# ho <- c("Atomic vector", "Matrix", "Array")
-# he <- c("List", "Data frame", "")
-# dt <- data.frame(Dimension = d, Homogeneous = ho, Heterogeneous = he, stringsAsFactors = FALSE)
-# kable(dt) %>%
-#   kable_styling(bootstrap_options = c("condensed", "hover"), full_width = FALSE, position = "left")
-# ```
 
 # TO DO - put in mapping between typeof and mode and storage.mode
 # hierarachy: NULL < raw < logical < integer < double < complex < character < list < expression
